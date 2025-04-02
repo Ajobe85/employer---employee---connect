6c4a6a1c6f6cd1261a16834e3ce7f27ee45ba55b
Ajobe85 <ajobe689@gmail.com>
2025-04-02 07:41:34 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,15 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";  
 
 // REPLACE BELOW WITH YOUR FIREBASE CONFIG  
-const firebaseConfig = {  
-  apiKey: "PASTE_HERE",  
-  authDomain: "PASTE_HERE",  
-  projectId: "PASTE_HERE",  
-  storageBucket: "PASTE_HERE",  
-  messagingSenderId: "PASTE_HERE",  
-  appId: "PASTE_HERE"  
-};  
-
-const app = initializeApp(firebaseConfig);  
-export const db = getFirestore(app);  
-export const auth = getAuth(app);  
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBD6Yba5P9mTdS_cW_D88x1MOmQ01LcWIQ",
+  authDomain: "employer-employee-connect.firebaseapp.com",
+  projectId: "employer-employee-connect",
+  storageBucket: "employer-employee-connect.firebasestorage.app",
+  messagingSenderId: "1008070246651",
+  appId: "1:1008070246651:web:9624e9c0f5d0075fcaf05e",
+  measurementId: "G-ZKSRHG72W7"
+};
