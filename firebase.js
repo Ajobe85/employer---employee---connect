17b259213174be5e428df0d86e6944f9c2c130c9
AJOBE SWALIKI <ajobe689@gmail.com>
2025-04-03 16:23:50 +0300

    Update firebase.js
    Signed-off-by: AJOBE SWALIKI  <ajobe689@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,16 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
 // REPLACE THE VALUES BELOW WITH YOUR FIREBASE CONFIG (from Firebase Console)
 const firebaseConfig = {
-  apiKey: "AIzaSyBx...", // Replace with your API key
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123def456"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
