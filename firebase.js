050af1b6a2dd227996bf3f7ab011995525c17f24
AJOBE SWALIKI <ajobe689@gmail.com>
2025-04-04 09:24:33 +0300

    Update firebase.js
    Signed-off-by: AJOBE SWALIKI  <ajobe689@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,6 +1,15 @@
 // firebase.js
 const firebaseConfig = {
-  
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBD6Yba5P9mTdS_cW_D88x1MOmQ01LcWIQ",
+  authDomain: "employer-employee-connect.firebaseapp.com",
+  projectId: "employer-employee-connect",
+  storageBucket: "employer-employee-connect.firebasestorage.app",
+  messagingSenderId: "1008070246651",
+  appId: "1:1008070246651:web:9624e9c0f5d0075fcaf05e",
+  measurementId: "G-ZKSRHG72W7"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
