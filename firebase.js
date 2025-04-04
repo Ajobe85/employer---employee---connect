13d13d99649f6b547872f2c013e5e25a03719c91
AJOBE SWALIKI <ajobe689@gmail.com>
2025-04-04 11:12:38 +0300

    Update firebase.js
    Signed-off-by: AJOBE SWALIKI  <ajobe689@gmail.com>

--- firebase.js
+++ firebase.js
@@ -2,6 +2,15 @@
 const firebaseConfig = {
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyBD6Yba5P9mTdS_cW_D88x1MOmQ01LcWIQ",
   authDomain: "employer-employee-connect.firebaseapp.com",
   projectId: "employer-employee-connect",
@@ -10,6 +19,10 @@ const firebaseConfig = {
   appId: "1:1008070246651:web:9624e9c0f5d0075fcaf05e",
   measurementId: "G-ZKSRHG72W7"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 hash_config {
   algorithm: SCRYPT,
   base64_signer_key: mUI5j2BmiFabZusYnPRnbNeFV5EGn7Co21ClN3pv8UvZ5Vx3jVz4Dwtfm+5FHHFJaC2KfpB8TpAa72UW//HEaQ==,
