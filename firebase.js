eaff20d78e3c6b20ff74068b2734a007cc44a4d0
AJOBE SWALIKI <ajobe689@gmail.com>
2025-04-04 09:18:49 +0300

    Update firebase.js
    Signed-off-by: AJOBE SWALIKI  <ajobe689@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,18 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-// REPLACE THE VALUES BELOW WITH YOUR FIREBASE CONFIG (from Firebase Console)
+// firebase.js
 const firebaseConfig = {
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBD6Yba5P9mTdS_cW_D88x1MOmQ01LcWIQ",
-  authDomain: "employer-employee-connect.firebaseapp.com",
-  projectId: "employer-employee-connect",
-  storageBucket: "employer-employee-connect.firebasestorage.app",
-  messagingSenderId: "1008070246651",
-  appId: "1:1008070246651:web:9624e9c0f5d0075fcaf05e",
-};
+  
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const database = firebase.database();
