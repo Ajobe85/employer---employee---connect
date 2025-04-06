834eb87ebeddc0ce52f437d4fa17df9d125b8f84
AJOBE SWALIKI <ajobe689@gmail.com>
2025-04-06 08:43:26 +0300

    Update firebase.js
    Signed-off-by: AJOBE SWALIKI  <ajobe689@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,21 +1,19 @@
-// firebase.js
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase.js (Version 8 Compatibility)
 const firebaseConfig = {
   apiKey: "AIzaSyBD6Yba5P9mTdS_cW_D88x1MOmQ01LcWIQ",
   authDomain: "employer-employee-connect.firebaseapp.com",
   projectId: "employer-employee-connect",
-  storageBucket: "employer-employee-connect.firebasestorage.app",
+  storageBucket: "employer-employee-connect.appspot.com",
   messagingSenderId: "1008070246651",
-  appId: "1:1008070246651:web:9624e9c0f5d0075fcaf05e",
-  measurementId: "G-ZKSRHG72W7"
+  appId: "1:1008070246651:web:9624e9c0f5d0075fcaf05e"
 };
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Initialize services
+const auth = firebase.auth();
+const database = firebase.database();
   
